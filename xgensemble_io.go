4daf73c1168f11e56b90332e4ba0be2d7eeec69a
koth <yovnchine@163.com>
2020-09-10 19:06:16 +0800

    Update xgensemble_io.go

--- xgensemble_io.go
+++ xgensemble_io.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/dmitryikh/leaves/internal/xgbin"
-	"github.com/dmitryikh/leaves/transformation"
+	"github.com/koth/leaves/internal/xgbin"
+	"github.com/koth/leaves/transformation"
 )
 
 func xgSplitIndex(origNode *xgbin.Node) uint32 {
